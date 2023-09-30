0e63a99a41f232ec7b752b2ebd2106cea005d074
Karan Vaswani <53395550+karan-17@users.noreply.github.com>
2023-10-01 00:53:21 +0530

    Updated Icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,10 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
         <title> Karan Vaswani Portfolio</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="manifest" href="/site.webmanifest">
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
